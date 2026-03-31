fc460e99e754cf0a8df13ce737e5f069e3175d46
david3478 <inwookim03@naver.com>
2026-03-31 18:46:13 +0900

    feat: 0331 수업 및 과제

---
+++ src/main/java/org/example/DrawingState.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public enum DrawingState {
+    NONE, DRAWING
+}
--- src/main/java/org/example/GDrawingPanel.java
+++ src/main/java/org/example/GDrawingPanel.java
@@ -2,48 +2,69 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
-import java.util.ArrayList;
-import java.util.List;
 
 public class GDrawingPanel extends JPanel {
 
+    public DrawingState drawingState;
+
     public GDrawingPanel() {
-        this.setBackground(Color.CYAN);
+        this.setBackground(Color.WHITE);
+
+        drawingState = DrawingState.NONE;
+
+        MouseHandler mouseHandler = new MouseHandler();
+        this.addMouseListener(mouseHandler);
+        this.addMouseMotionListener(mouseHandler);
     }
 
-    private void startDrawing(int x, int y) {
-        Graphics graphics = this.getGraphics();
-        graphics.drawRect(x, y, this.getWidth(), this.getHeight());
+    private int x0,y0;
+    private int x1,y1;
+
+    private void startRectangularShape(int x, int y) {
+        this.x0 = x;
+        this.y0 = y;
+
+        this.drawingState = DrawingState.DRAWING;
     }
     private void keepDrawing(int x, int y) {
 
     }
-    private void stopDrawing(int x, int y) {
+    private void finishRectangularShape(int x, int y) {
+        this.x1 = x;
+        this.y1 = y;
+
+        Graphics2D g2d = (Graphics2D) this.getGraphics();
+        g2d.drawRect(this.x0, this.y0, this.x1-this.x0, this.y1-this.y0);
+
+        this.drawingState = DrawingState.NONE;
     }
 
     private class MouseHandler implements MouseListener,MouseMotionListener {
 
         @Override
         public void mouseClicked(MouseEvent e) {
-
+            if(e.getClickCount()==2 && drawingState==DrawingState.DRAWING){
+                finishRectangularShape(e.getX(),e.getY());
+            } else if (e.getClickCount()==1 && drawingState==DrawingState.NONE) {
+                startRectangularShape(e.getX(),e.getY());
+            }
         }
 
         @Override
         public void mousePressed(MouseEvent e) {
-            startDrawing(e.getX(),e.getY());
+//            startRectangularShape(e.getX(),e.getY());
         }
 
         @Override
         public void mouseDragged(MouseEvent e) {
-            keepDrawing(e.getX(),e.getY());
+//            finishRectangularShape(e.getX(),e.getY());
         }
         @Override
         public void mouseReleased(MouseEvent e) {
-            stopDrawing(e.getX(),e.getY());
+//            finishRectangularShape(e.getX(),e.getY());
         }
 
         @Override
