07452a7f597b041e30908e8d37eab1e1b5f31a16
david3478 <inwookim03@naver.com>
2026-04-14 15:00:37 +0900

    feat: 0407 lecture add

--- src/main/java/org/example/shape/GShape.java
+++ src/main/java/org/example/shape/GShape.java
@@ -2,45 +2,60 @@ package org.example.shape;
 
 import java.awt.*;
 
-public abstract class GShape {
-    protected int x;
-    protected int y;
-    protected int width;
-    protected int height;
+public class GShape {
+    protected int x0;
+    protected int y0;
+    protected int x1;
+    protected int y1;
 
     public GShape(int x, int y, int width, int height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+        this.x0 = x;
+        this.y0 = y;
+        this.x1 = width;
+        this.y1 = height;
     }
 
+    public void setLocation0(int x, int y) {
+        this.x0 = x;
+        this.y0 = y;
+    }
+    public void setLocation1(int x, int y) {
+        this.x1 = x;
+        this.y1 = y;
+    }
 
-    public abstract void draw(Graphics g);
+    public void setSize(int width, int height) {
+        this.x1 = x0 + width;
+        this.y1 = y0 + height;
+    }
+    public void draw(Graphics2D g) {
+
+    }
+
 
     // getter
-    public int getX() {
-        return x;
+    public int getX0() {
+        return x0;
     }
 
-    public int getY() {
-        return y;
+    public int getY0() {
+        return y0;
     }
 
-    public int getWidth() {
-        return width;
+    public int getX1() {
+        return x1;
     }
 
-    public int getHeight() {
-        return height;
+    public int getY1() {
+        return y1;
     }
 
     // setter
-    public void setWidth(int width) {
-        this.width = width;
+    public void setX1(int x1) {
+        this.x1 = x1;
     }
 
-    public void setHeight(int height) {
-        this.height = height;
+    public void setY1(int y1) {
+        this.y1 = y1;
     }
 }
